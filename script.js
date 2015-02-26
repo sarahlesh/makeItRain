d31016948cda9453c6331d996d4545abf67242d3
Liz Lesh <lizlesh@701AMAC01.local>
2015-02-26 14:17:07 -0500

    added functionality in .js, added styles

--- script.js
+++ script.js
@@ -1,39 +1,19 @@
-$(function() {
+$(function(){
+	var maxBills = 100;
+
+	// var randomNumber = Math.floor(Math.random()*1500);
+
+	for (var i = 0; i < maxBills; i++){
+
+	var randomNumber = Math.floor(Math.random()*1500);
 	
-	var maxBills = 100; //max amount of bills per page
-	
-	// var billsArray = [];
+	var bills = $("<span>")
+		.addClass('billsBillsBills')
+		.text("$")
+		.css({
+			left : randomNumber
+		});
 
-	//generate random number
-
-	for (var i = 0; i < maxBills ; i ++){
-
-		var randomNumber = Math.floor(Math.random()*1500);
-		console.log(randomNumber)
-
-		var bills = "<span class='billsBillsBills' style = 'left:" + randomNumber +"px' >$</span>";
-		$('body').append(bills);//append spans to page
-
-
-		// $("span.billsBillsBills")
-		}
-
-
-
-
+	$('body').append(bills);
+	}
 });
-
-		//make all arrays display randomly ontop of page
-
-
-		// $('span').addClass("billsBillsBills"*randomNumber);
-
-	// create an div and add to page
-
-
-
-
-
-
-
-
